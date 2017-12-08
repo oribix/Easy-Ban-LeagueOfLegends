d6509e21a878fafaa13ce8d2df16858ee7e3d074
Marco Morelos <iammarcomorelos@gmail.com>
2017-12-08 15:28:25 -0800

    Fixed bugs not allowing the script to work in python 3.

--- easyban.py
+++ easyban.py
@@ -1,7 +1,10 @@
 import sys
 import json
-import urllib2
 import re
+try:
+  import urllib.request as urllib2
+except ImportError:
+  import urllib2
 
 #constants
 CHAMPION = 0
@@ -40,12 +43,12 @@ def parseArgs(argv):
     else:
       global ELO
       ELO = arg
-  
+
   #set short flags
   for arg in shortargs:
     for flag in arg:
       setFlag(flag)()
-  
+
   #set long flags
   for arg in longargs:
     print( arg)
@@ -59,7 +62,7 @@ def printStats(champions):
     c = None
     if(len(champions) > 0):
       c = champions.pop()
-      
+
     if(c != None):
       print( "champion:  " + c[CHAMPION])
       print( "THREAT LEVEL: " + "%.6f" % (c[THREAT] * 100) + "%")
@@ -68,25 +71,25 @@ def printStats(champions):
         print( "ban rate:  " + "%.2f" % (c[BANRATE] * 100) + "%")
         print( "pick rate: " + "%.2f" % (c[PICKRATE] * 100) + "%")
         print( "true pick: " + "%.2f" % (c[TRUEPICKRATE] * 100) + "%")
-      
-      print
-  
+
+      print("")
+
 def main():
   args = sys.argv
   args.pop(0)
   parseArgs(args)
-  
+
   statbase = "http://champion.gg/statistics/"
   query = "?league=" + ELO + "#?sortBy=general.playPercent&order=descend"
   url = statbase + query
-  
+
   response = urllib2.urlopen(url)
   html = response.read()
-  
-  match = re.search(r'matchupData.stats = ([^\n]*\])', html)
-  
+
+  match = re.search(r'matchupData.stats = ([^\n]*\])', html.decode("utf-8"))
+
   stats = json.loads(match.group(1))
-  
+
   champions = []
   for item in stats:
     champion = item["key"]
@@ -94,20 +97,20 @@ def main():
     banRate = item["general"]["banRate"]
     pickRate = item["general"]["playPercent"]
     winRate = item["general"]["winPercent"]
-    
+
     #calculate threat level
     truePickRate = pickRate/(1.0-banRate)
     threat = truePickRate * (winRate - 0.5) + 0.5
-    
+
     champwithrole = champion + "(" + role + ")"
-    
+
     elem = [champwithrole, winRate, banRate, pickRate, truePickRate, threat];
-    
+
     champions.append(elem)
-  
+
   champions.sort(key=threatgetter)
-  
+
   printStats(champions)
-  
+
 if __name__ == "__main__":
   main()
