f5ca7c36b570a57091e4c25e85ca15c0178d6470
Marco Morelos <iammarcomorelos@gmail.com>
2018-02-01 21:47:02 -0800

    Illegal flags now fail gracefully

--- easyban.py
+++ easyban.py
@@ -27,9 +27,13 @@ def setVerbose():
   return
 
 def setFlag(f):
-  return{
-    "v" : setVerbose
-  }[f]
+  try:
+    {
+      "v": setVerbose
+    }[f]()
+  except KeyError:
+    print("No such flag '" + f + "'")
+    exit()
 
 def parseArgs(argv):
   arglen = len(argv)
@@ -49,15 +53,15 @@ def parseArgs(argv):
         RESULTNUM = rn
         continue
       except ValueError: pass
-      
+
       global ELO
       ELO = arg
-  
+
   #set short flags
   for arg in shortargs:
     for flag in arg:
-      setFlag(flag)()
-  
+      setFlag(flag)
+
   #set long flags
   for arg in longargs:
     continue
@@ -71,7 +75,7 @@ def printStats(champions):
     c = None
     if(len(champions) > 0):
       c = champions.pop()
-      
+
     if(c != None):
       print( "champion:  " + c[CHAMPION])
       print( "THREAT LEVEL: " + "%.6f" % (c[THREAT] * 100) + "%")
@@ -80,25 +84,30 @@ def printStats(champions):
         print( "ban rate:  " + "%.2f" % (c[BANRATE] * 100) + "%")
         print( "pick rate: " + "%.2f" % (c[PICKRATE] * 100) + "%")
         print( "true pick: " + "%.2f" % (c[TRUEPICKRATE] * 100) + "%")
-      
+
       print("")
-  
+
 def main():
+  #TODO: Get default options from a file
+
+  #parse arguments
   args = sys.argv
   args.pop(0)
   parseArgs(args)
-  
+
+  #generate the URL to query
   statbase = "http://champion.gg/statistics/"
   query = "?league=" + ELO + "#?sortBy=general.playPercent&order=descend"
   url = statbase + query
-  
+
+  #Get r
   response = urllib2.urlopen(url)
   html = response.read()
-  
+
   match = re.search(r'matchupData.stats = ([^\n]*\])', html.decode("utf-8"))
-  
+
   stats = json.loads(match.group(1))
-  
+
   champions = []
   for item in stats:
     champion = item["key"]
@@ -106,20 +115,20 @@ def main():
     banRate = item["general"]["banRate"]
     pickRate = item["general"]["playPercent"]
     winRate = item["general"]["winPercent"]
-    
+
     #calculate threat level
     truePickRate = pickRate/(1.0-banRate)
     threat = truePickRate * (winRate - 0.5) + 0.5
-    
+
     champwithrole = champion + "(" + role + ")"
-    
+
     elem = [champwithrole, winRate, banRate, pickRate, truePickRate, threat];
-    
+
     champions.append(elem)
-  
+
   champions.sort(key=threatgetter)
-  
+
   printStats(champions)
-  
+
 if __name__ == "__main__":
   main()
